1291d4357bf3905467f1af4658f09779ac6351f2
juanitogamer313 <150815385+juanitogamer313@users.noreply.github.com>
2024-01-16 08:22:57 -0600

    Implementación de la interfaz HabilidadDeEfecto

--- Interfaces/IHabilidadDeEfecto.cs
+++ Interfaces/IHabilidadDeEfecto.cs
@@ -3,10 +3,6 @@ namespace ExamenExtra_FundamentosSoftware_JuanCervantes.interfaces;
 public interface IHabilidadDeEfecto : IHabilidad
 {
 
-    int CostoMana { get; }
-    bool AutoAplicable { get; }
-    int Potencia { get; }
-    TipoEfecto TipoEfecto { get; }
-    void Lanzar(IPersonaje lanzador, IPersonaje objetivo);
+    void AplicarEfecto(IPersonaje objetivo);
 
 }
