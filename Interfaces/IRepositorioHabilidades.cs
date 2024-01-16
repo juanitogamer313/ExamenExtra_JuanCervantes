7ec5f3cb8d2b3a30321198db19a2c9c0ed389b77
juanitogamer313 <150815385+juanitogamer313@users.noreply.github.com>
2024-01-16 08:26:23 -0600

    Implementación de la interfaz RepositorioHabilidades

--- Interfaces/IRepositorioHabilidades.cs
+++ Interfaces/IRepositorioHabilidades.cs
@@ -2,5 +2,10 @@ namespace ExamenExtra_FundamentosSoftware_JuanCervantes.interfaces;
 
 public interface IRepositorioHabilidades
 {
-    
+
+    IHabilidad BuscarHabilidad(string id);
+    List<IHabilidad> BuscarHabilidades(ClaseHabilidad clase, int nivelRequerido);
+    List<IHabilidad> ObtenerTodasLasHabilidades();
+    void AgregarHabilidad(IHabilidad habilidad);
+
 }
