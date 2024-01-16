bfb8a009928ec9c791f575f160bbf05f7a932075
juanitogamer313 <150815385+juanitogamer313@users.noreply.github.com>
2024-01-16 00:01:40 -0600

    gitignore

---
+++ ExamenExtra_FundamentosSoftware-JuanCervantes/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace ExamenExtra_FundamentosSoftware_JuanCervantes
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
